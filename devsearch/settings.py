1388319928b2be3c21240b0ca0cfd15007a20ab1
tdhung03 <mspotify208@gmail.com>
2023-08-12 16:31:25 +0700

    root template

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "devsearch.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our products")
+    return render(request, 'projects.html')
 
 
 def project(request, pk):
-    return HttpResponse('Single project' + ' ' + str(pk))
+    return render(request, 'single-project.html')
