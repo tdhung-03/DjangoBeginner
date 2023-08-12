a3f7e19fac93c0b65df1ec5e40d018355def3fb3
tdhung03 <mspotify208@gmail.com>
2023-08-12 11:43:24 +0700

    url routing sample

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse("Here are our products")
+
+
+def project(request, pk):
+    return HttpResponse('Single project' + ' ' + str(pk))
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("projects/", projects, name="projects"),
+    path("project/<str:pk>", project, name="project")
 ]
