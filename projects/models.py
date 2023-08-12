a3c6391183214d1901c3b45ffa34fc6989909644
tdhung03 <mspotify208@gmail.com>
2023-08-12 21:58:31 +0700

    __str__ for project

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,6 @@ class Project(models.Model):
     source_link = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
