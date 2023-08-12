34dbf31cfa4664a6f489f9717eadbdc64b23a7ff
tdhung03 <mspotify208@gmail.com>
2023-08-12 18:57:38 +0700

    render data

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+
+    {'id': '1', 'title': 'Ecommerce Website', 'description': 'Fully functional ecommerce website'},
+
+    {'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work'},
+
+    {'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community'}
+
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 10
+    context = {
+        'page': page,
+        'number': number,
+        'projects': projectsList,
+    }
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for project in projectsList:
+        if project['id'] == pk:
+            projectObj = project
+    return render(request, 'projects/single-project.html', {"project": projectObj})
