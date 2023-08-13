d7c328611e8dc3edfd261497d93e46af9e6ea5ce
tdhung03 <mspotify208@gmail.com>
2023-08-13 21:47:47 +0700

    clean code

--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import *
-
-projectsList = [
-
-    {'id': '1', 'title': 'Ecommerce Website', 'description': 'Fully functional ecommerce website'},
-
-    {'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work'},
-
-    {'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community'}
-
-]
 
 
 def projects(request):
