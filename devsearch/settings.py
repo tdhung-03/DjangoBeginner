ba0205303db98c5303e5e13a214f160836abd5ed
tdhung03 <mspotify208@gmail.com>
2023-08-17 10:40:17 +0700

    config mediaroot(for serving)

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -116,6 +116,8 @@ STATICFILES_DIRS = (
     BASE_DIR / "static",
 )
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
